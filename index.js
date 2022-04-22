3b7e96f99558e86eaed45dc99d997186db31c714
yesim <sityesim@gmail.com>
2022-04-22 20:15:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(arr){
+    const result=arr.find(arr =>arr.result === "W");
+    if (result) {
+        return result.year;
+    }else{
+        return undefined;
+    };
+};
